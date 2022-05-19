8707754dda730c3b5a0f75e839bcb78a3195991c
weibin.shao <gis_swb@163.com>
2022-05-19 17:23:03 +0800

    chore: prettier config

---
+++ vue3-template/.prettierrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+  semi: false, // 行位是否使用分号，默认为 true
+  trailingComma: 'all', // 是否使用尾逗号，有三个可选值 "<none|es5|all>"
+  singleQuote: true, // 字符串是否使用单引号，默认为 false，使用双引号
+  printWidth: 100, // 一行的字符数，如果超过会进行换行，默认为 80
+  bracketSpacing: true, // 对象大括号直接是否有空格，默认为true，效果：{ foo: bar }
+}
